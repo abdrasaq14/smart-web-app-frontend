c20a7ec9360267db2c4cc216367a330cc81c5919
vasicolibridigital <99743133+vasicolibridigital@users.noreply.github.com>
2022-10-21 11:46:06 +0300

    show companies and employess in tables

--- src/api/accountUI/users/index.ts
+++ src/api/accountUI/users/index.ts
@@ -1,4 +1,4 @@
-import { ApiUsers, User, UserResponseSchema } from './types';
+import { ApiUsers, UserResponseSchema } from './types';
 import { mockResponse } from './mock';
 import { getDashboardData } from '../../apiUtils';
 import { useQuery } from 'react-query';
@@ -16,4 +16,5 @@ const getUsers = getDashboardData<ApiUsers>({
 export const useGetUsers = (options?: DashboardQueryProps) =>
 	useQuery([apiRoute, options], () => getUsers(options));
 
-export const getUserName = (user: User) => `${user.first_name} ${user.last_name}`;
+export const getUserName = (user: { first_name: string; last_name: string }) =>
+	`${user.first_name} ${user.last_name}`;
--- src/view/accountUI/Companies.tsx
+++ src/view/accountUI/Companies.tsx
@@ -3,10 +3,8 @@ import { Box } from '@mui/material';
 import { SitesDashboardFilters } from '../../types';
 import { DEFAULT_DASHBOARD_FILTERS } from '../../utils/constants';
 import { SearchHeader } from '../../layouts/SearchHeader';
-import { useGetCompanies } from '../../api/accountUI/companies';
-import { Spinner } from '../../components/Spinner';
-import { Link } from 'react-router-dom';
 import AddCompanyForm from '../../components/Forms/AddCompanyForm';
+import { CompaniesTable } from '../../components/Tables/CompaniesTable';
 
 const styles = {
 	screenContent: {
@@ -19,13 +17,6 @@ const styles = {
 
 export const Companies = () => {
 	const [filters, setFilters] = useState<SitesDashboardFilters>(DEFAULT_DASHBOARD_FILTERS);
-	const { data: companies, isLoading, isError } = useGetCompanies({ filters });
-
-	if (isLoading) {
-		return <Spinner />;
-	} else if (isError) {
-		return <Box>Error fetching data...</Box>;
-	}
 
 	return (
 		<Box sx={styles.screenContent}>
@@ -36,18 +27,7 @@ export const Companies = () => {
 			/>
 			<Box sx={{ display: 'flex', justifyContent: 'space-between', paddingTop: '32px' }}>
 				<Box>
-					{companies?.results?.map((company) => (
-						<Box key={company.id} sx={{ padding: '32px' }}>
-							<Box sx={{ padding: '32px' }}>
-								<Link to={company.id.toString()}>{company.name}</Link>
-							</Box>
-							<ul>
-								{company.users.map((user) => (
-									<li>{`${user.name}  ${user.date}`}</li>
-								))}
-							</ul>
-						</Box>
-					))}
+					<CompaniesTable filters={filters} />
 				</Box>
 				<Box sx={{ width: '320px' }}>
 					<AddCompanyForm />
