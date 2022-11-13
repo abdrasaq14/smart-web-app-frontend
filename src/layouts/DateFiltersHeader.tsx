30375c0d216a756ae288ac215a5eed5cd5c6f537
vasicolibridigital <99743133+vasicolibridigital@users.noreply.github.com>
2022-11-13 11:46:28 +0200

    remove print buttons

--- src/layouts/DateFiltersHeader.tsx
+++ src/layouts/DateFiltersHeader.tsx
@@ -1,7 +1,6 @@
 import { Grid } from '@mui/material';
 import React from 'react';
 import ControlledDatePicker from '../components/ControlledDatePicker';
-import { RegularButton } from '../components/Button';
 import { SitesDashboardFilters } from '../types';
 import { Header } from './Header';
 
@@ -17,24 +16,24 @@ const DateFiltersHeader = ({ filters, setFilters }: Props) => {
 
 	return (
 		<Header>
-			<Grid container spacing={1} sx={{ width: '650px' }}>
-				<Grid item lg={4} md={4} sm={12} xs={12}>
+			<Grid container spacing={1} sx={{ width: '400px' }}>
+				<Grid item lg={6} md={6} sm={12} xs={12}>
 					<ControlledDatePicker
 						label="Start Date"
 						value={filters.start_date ?? null}
 						setValue={updateFilters('start_date')}
 					/>
 				</Grid>
-				<Grid item lg={4} md={4} sm={12} xs={12}>
+				<Grid item lg={6} md={6} sm={12} xs={12}>
 					<ControlledDatePicker
 						label="End Date"
 						value={filters.end_date ?? null}
 						setValue={updateFilters('end_date')}
 					/>
 				</Grid>
-				<Grid item lg={4} md={4} sm={12} xs={12}>
-					<RegularButton label="Print" onClick={() => {}} />
-				</Grid>
+				{/*<Grid item lg={4} md={4} sm={12} xs={12}>*/}
+				{/*	<RegularButton label="Print" onClick={() => {}} />*/}
+				{/*</Grid>*/}
 			</Grid>
 		</Header>
 	);
