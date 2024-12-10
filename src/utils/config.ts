3282dca0966f7dab463615f4809c7ecaaa97fc49
smartie-abdrasaq <a.suleiman@smarterise.com>
2024-12-10 15:00:51 +0100

    modified the web pack config

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,9 +1,9 @@
 console.log('process.env.REACT_APP_BASE_URL', process.env.REACT_APP_BASE_URL);
 export const Config = {
-	BASE_URL: process.env.REACT_APP_BASE_URL,
-	AUTH0_CLIENT_ID: process.env.REACT_APP_AUTH0_CLIENT_ID,
-	AUTH0_DOMAIN: process.env.REACT_APP_AUTH0_DOMAIN,
-	AUTH0_AUDIENCE: process.env.REACT_APP_AUTH0_API_AUDIENCE,
+	BASE_URL: process.env.REACT_APP_BASE_URL || 'https://api.demo.powersmarter.net/api/',
+	AUTH0_CLIENT_ID: process.env.REACT_APP_AUTH0_CLIENT_ID || 'RLHdjXQH7M3j8Tj1ygx7t8YZ0jgZsnxH',
+	AUTH0_DOMAIN: process.env.REACT_APP_AUTH0_DOMAIN || 'dev-mgw72jpas4obd84e.us.auth0.com',
+	AUTH0_AUDIENCE: process.env.REACT_APP_AUTH0_API_AUDIENCE || 'https://api.demo.powersmarter.net/',
 	// AUTH0_SECRET_KEY: process.env.REACT_APP_AUTH0_CLIENT_SECRET_KEY,
 };
 
