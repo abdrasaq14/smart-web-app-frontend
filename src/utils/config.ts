f24d084059b954aa50b3fd2b3afee6a2e4b9cfff
smartie-abdrasaq <a.suleiman@smarterise.com>
2024-12-10 13:54:49 +0100

    added the environment variables to the deploy.yml

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,3 +1,4 @@
+console.log('process.env.REACT_APP_BASE_URL', process.env);
 export const Config = {
 	BASE_URL: process.env.REACT_APP_BASE_URL,
 	AUTH0_CLIENT_ID: process.env.REACT_APP_AUTH0_CLIENT_ID,
