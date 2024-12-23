ab35ad82c7fad4457b0b26ce1dadd88ddb21feff
abdrasaq14 <abdrasaq14@gmail.com>
2024-12-23 13:17:43 +0100

    dashboard embedgging

--- src/view/sbee.tsx
+++ src/view/sbee.tsx
@@ -1,90 +1,113 @@
+import React, { useEffect, useRef, useState } from 'react';
 import { Box, Card } from '@mui/material';
-import React from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
-import { RegularButton } from '../components/Button';
-import { SIDEBAR_WIDTH } from '../utils/constants';
+import { createEmbeddingContext } from 'amazon-quicksight-embedding-sdk';
+import './sbee.css';
+import { post } from '../api/apiUtils';
+import Loader from '../components/Loader/Loader';
 
 const styles = {
 	container: {
 		height: '100%',
+		width: '100%',
 		display: 'flex',
 		justifyContent: 'center',
 		alignItems: 'center',
-		// backgroundColor: 'red',
 	},
 	loginCard: {
 		display: 'flex',
 		flexDirection: 'column',
 		justifyContent: 'space-between',
 		alignItems: 'center',
-		maxWidth: '550px',
-		minWidth: 343,
-		height: 150,
+		maxWidth: '90%',
+		width: '100%',
+		height: '90%',
 		padding: '40px',
 		borderRadius: '16px',
-		marginTop: '-100px',
 		borderColor: '#E6E6E6',
 		backgroundColor: '#F7F7F7',
 	},
-	title: { fontSize: '24px', fontWeight: 500, marginBottom: '20px' },
-	helperRow: {
-		display: 'flex',
-		justifyContent: 'space-between',
-		alignItems: 'center',
-		width: '100%',
-	},
-	forgotPassButton: {
-		cursor: 'pointer',
-	},
-	logo: { cursor: 'pointer', width: SIDEBAR_WIDTH },
+	logo: { cursor: 'pointer', maxWidth: '170px' },
 };
 
-const LoginButton = () => {
-    const handlecontinue = () => { 
-        window.location.href =
-					'https://eu-west-2.quicksight.aws.amazon.com/embed/ee9de79629594ec7afff7c64cc1d8c68/dashboards/53d6cc6b-7454-4ae1-8702-a5b7142aafa1?identityprovider=quicksight&isauthcode=true&code=AYABeBO14dvlm7G7mcvK46f-688AAAABAAdhd3Mta21zAEthcm46YXdzOmttczpldS13ZXN0LTI6NjQ5MTA0MjA5NDE4OmtleS9lZGQ0ZjBlNC03M2U0LTQ4NGEtYjBmYy0yYjEzNDI0YThlODUAuAECAQB40arshFNPPoBUAbB9hRxqalRsdpuiVanS5r6RYzejOEIBrBmHvaZXgfdx4P2cxEYHNwAAAH4wfAYJKoZIhvcNAQcGoG8wbQIBADBoBgkqhkiG9w0BBwEwHgYJYIZIAWUDBAEuMBEEDLweJfwVTxsLrNFuQAIBEIA788bbgXBDP6o80-dm93jL5OTpE86gJ8PLtmP5gLOiZTiMpgUi3FzjeMAW1FtgcUK7FLXgc38rzzFIiAECAAAAAAwAABAAAAAAAAAAAAAAAAAA4MZq5q22qEcgINBeFrP1-v____8AAAABAAAAAAAAAAAAAAABAAAAm4Y1Sikd7RrSuZA7clvV33PDyIvGv5O2Nn5sf_iTwfegQk72huTA03hy52Ja2CT20sQiewQ6mL31vussvaEPCmGePtRSfHdywyWWWSeB47LwMfzOtDGgNpCojdExi1tUjQIxkp72jsrVIgC_ifySiprY4ED4AAYkfIjvLHamNb2xpHCY8PGLJ7ezd7R2atyYznq-CwnbdwsTm7LvusiLCOwTj91uFd8Tv1DHQw%3D%3D';
-    }
+const DashboardEmbed = () => {
+	const containerRef = useRef<HTMLDivElement>(null);
+	const [embedUrl, setEmbedUrl] = useState<string | null>(null);
+	const [isLoading, setIsLoading] = useState(true);
+	const [error, setError] = useState<string | null>(null);
+	// Fetch the embed URL from the backend
+	const fetchEmbedUrl = async () => {
+		try {
+			const response = await post('generate-embed-url', {
+				accountId: '368563411071',
+				dashboardId: '1e7e705c-7dd4-4226-bb44-1b4da8108716',
+				namespace: 'default',
+				authorizedResourceArns: [
+					`arn:aws:quicksight:eu-west-2:368563411071:dashboard/1e7e705c-7dd4-4226-bb44-1b4da8108716`,
+				],
+				// authorizedResourceArns: ['arn:aws:quicksight:region:account-id:dashboard/dashboard-id'],
+				allowedDomains: ['http://localhost:3000', 'https://demo.powersmarter.net'],
+				sessionTags: [],
+			});
+			if (response.EmbedUrl) {
+				setEmbedUrl(response.EmbedUrl);
+				setIsLoading(false);
+				return
+			}
+			setError('Error while fetching your dashboard. Please try again later.');
+			setIsLoading(false);
+		} catch (error) {
+			setError('Error while fetching your dashboard. Please try again later.');
+			setIsLoading(false);
+			console.error('Error fetching embed URL:', error);
+		}
+	};
+
+	// Embed the QuickSight dashboard
+	useEffect(() => {
+		if (!embedUrl) return;
+
+		const embedQuickSight = async () => {
+			try {
+				const embeddingContext = await createEmbeddingContext();
+				const { embedDashboard } = embeddingContext;
+
+				embedDashboard({
+					url: embedUrl,
+					container: containerRef.current!,
+					width: '100%',
+					height: '700px',
+					className: 'quicksight-embedding-iframe',
+				});
+			} catch (error) {
+				console.error('Error embedding QuickSight:', error);
+			}
+		};
+
+		embedQuickSight();
+	}, [embedUrl]);
+
+	useEffect(() => {
+		fetchEmbedUrl();
+	}, []);
 
 	return (
-		<RegularButton
-			extraStyles={{
-				backgroundColor: '#FFC000',
-				color: '#000000',
-				'&:hover': {
-					backgroundColor: '#FFC000',
-					borderRadius: '8px',
-					color: '#000000',
-				},
-			}}
-			label="Login"
-			onClick={handlecontinue}
-		/>
-	);
-};
-
-export const SBEE = () => {
-	return (
-		<Box sx={{ display: 'flex', flexDirection: 'column', padding: '40px' }}>
-			<Box sx={{ height: '133px' }}>
+		<Box sx={{ display: 'flex', flexDirection: 'column', padding: '40px', gap: '100px' }}>
+			<Box sx={{ height: '120px' }}>
 				<img src="logo.png" alt="Smarterise" style={styles.logo} />
 			</Box>
 			<Box sx={styles.container}>
-				<Card sx={styles.loginCard} variant="outlined">
-					<Box sx={styles.title}>Click the button below to continue</Box>
-					{/*<TextField sx={{ width: '100%' }} id="user" type="text" placeholder='Email'/>*/}
-					{/*<TextField sx={{ width: '100%' }} id="password" type="password" placeholder='Password'/>*/}
-					{/*<Box sx={styles.helperRow}>*/}
-					{/*    <FormControlLabel*/}
-					{/*        control={<Checkbox checked={rememberMe} onChange={() => setRememberMe(!rememberMe)}/>}*/}
-					{/*        label="Remember me"*/}
-					{/*    />*/}
-					{/*    <Box sx={styles.forgotPassButton} onClick={() => {*/}
-					{/*    }}>Forgot Password?*/}
-					{/*    </Box>*/}
-					{/*</Box>*/}
-					<LoginButton />
-				</Card>
+				{isLoading ? (
+					<Loader />
+				) : error?.trim() ? (
+					<p style={{ fontSize: '28px', color: 'red' }}>{error}</p>
+				) : (
+					<Card sx={styles.loginCard}>
+						<Box ref={containerRef} style={{ width: '100%', height: '700px' }} />
+					</Card>
+				)}
 			</Box>
 		</Box>
 	);
 };
+
+export default DashboardEmbed;
