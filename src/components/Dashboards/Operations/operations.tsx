f69ff77c29bfa31f713ed051d0a5dfb6c5b86379
abdrasaq14 <abdrasaq14@gmail.com>
2025-02-01 15:10:05 +0100

    operations tab api integrations

--- src/components/Dashboards/Operations/operations.tsx
+++ src/components/Dashboards/Operations/operations.tsx
@@ -1,9 +1,13 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import StatsCard from "../../Cards/statsCard";
 import { CARD_GAP } from "../../../utils/constants";
 import DoughNutChart from "../../Charts/SiteMonitored";
 import LoadProfileChart from "../../Charts/LoadProfileChart";
 import PowerConsumptionChart from "../../Charts/PowerConsumption";
 import AlertHistoryTable from "../../Table/AlertHistoryTable";
+import { useFetchData } from "../../../customHooks/useGetDashboardData";
+import { useMemo } from "react";
 
 export const dummyData = [
   {
@@ -31,8 +35,32 @@ export const dummyData = [
     isError: false,
   },
 ];
-
+const excludeCards = [
+  "Unknown",
+  "Number of Users",
+  "Number of Sites",
+  "Pending Alerts",
+];
 function Operations({ company_id }: { company_id: string }) {
+    const { data, isLoading, error } = useFetchData(
+      "/operations/cards-data",
+      {
+        card_type: "operations_home",
+        company_id,
+      },
+      {
+        // @ts-ignore
+        refreshInterval: 100000,
+        refetchOnMount: true,
+      }
+  );
+    const transformedData:any = useMemo(() => {
+      if (data) {
+        return data;
+      }
+      return null;
+    }, [company_id, data]);
+    console.log('ooperationsData', transformedData)
   return (
     <div
       className="flex flex-col items-start justify-center w-full"
@@ -46,16 +74,40 @@ function Operations({ company_id }: { company_id: string }) {
           gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr))",
         }}
       >
-        {dummyData.map((data, index) => (
-          <div key={index}>
-            <StatsCard
-              title={data.title}
-              value={data.value}
-              isError={data.isError}
-              isLoading={data.isLoading}
-            />
-          </div>
-        ))}
+        {isLoading
+          ? Array.from({ length: 4 }).map((_, index) => (
+              <StatsCard
+                key={index}
+                title=""
+                value=""
+                isError={false}
+                isLoading={true}
+              />
+            ))
+          : error
+          ? Array.from({ length: 4 }).map((_, index) => (
+              <StatsCard
+                key={index}
+                title=""
+                value=""
+                isError={true}
+                isLoading={false}
+                cardErrorStyle="!h-[50px]"
+              />
+            ))
+          : transformedData && transformedData.map(
+              (data:any, index:any) =>
+                !excludeCards.includes(data.label) && (
+                  <div key={index}>
+                    <StatsCard
+                      title={data.label}
+                      value={data.value as string}
+                      isError={!!error && !isLoading && !data}
+                      isLoading={isLoading}
+                    />
+                  </div>
+                )
+            )}
       </div>
 
       {/* chats wrapper */}
